cd5392d19e5ab3b78389b0435f51c0b807b512e6
Albedith Diaz <albedith@saucelabs.com>
2015-10-07 14:25:21 -0700

    Python test with Travis CI

---
+++ travis_test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import time
+import os
+from selenium import webdriver
+import sys
+
+# Retreiving enviroment variables
+SAUCE_USERNAME = os.environ.get('SAUCE_USERNAME')
+SAUCE_ACCESS_KEY = os.environ.get('SAUCE_ACCESS_KEY')
+
+desired_capabilities = {}
+desired_capabilities['platform'] = 'linux'
+desired_capabilities['version'] = '44'
+desired_capabilities['browserName'] = 'Chrome'
+
+driver = webdriver.Remote(command_executor = ('http://' + SAUCE_USERNAME + ':' + SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub'), desired_capabilities = self.desired_capabilities)
+driver.implicitly_wait(30)    
+
+driver.get('http://google.com') 
+title = driver.title
+assertEquals("Google", title)  
+
+time.sleep(10)
+
+driver.get('https://www.saucelabs.com')
+title = driver.title
+assertEquals("Sauce Labs: Selenium Testing, Mobile Testing, JS Unit Testing and More", title) 
+
+time.sleep(10)
+
+driver.get('http://www.theuselessweb.com/')
+title = driver.title
+assertEquals("The Useless Web", title) 
+
+driver.quit()
